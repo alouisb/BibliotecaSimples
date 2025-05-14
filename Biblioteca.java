a74ef60d37d332b7d5b45a6c7451741b223ae6f1
alouisb <alouisb2003@hotmail.com>
2025-05-13 21:45:16 -0300

    fix: adiciona documentação para busca case-insensitive

--- Biblioteca.java
+++ Biblioteca.java
@@ -13,6 +13,12 @@ public class Biblioteca {
         return new ArrayList<>(livros);
     }
 
+    /**
+     * Busca livros por autor, ignorando maiúsculas/minúsculas.
+     *
+     * @param autor Nome do autor a ser buscado.
+     * @return Lista de livros do autor especificado.
+     */
     public List<Livro> buscarPorAutor(String autor) {
         return livros.stream()
             .filter(livro -> livro.getAutor().equalsIgnoreCase(autor))
